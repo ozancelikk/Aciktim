5e47b0a9184804a05a0419649faa3de91716c2c5
KeyWteRn <eren.knl.ozdmr@gmail.com>
2023-03-29 16:06:53 +0300

    Entities ve sınıflar
    User,RestorantMenu,RestorantCategory,Restorant Sınıfları ve property ler eklendi

---
+++ Entities/Concrete/Restaurant.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+	public class Restaurant:IEntity
+	{
+		public string Id { get; set; }
+		public string? Name { get; set; }
+		public string? Taxnumber { get; set; }
+		public string? Address { get; set; }
+		public int RestaurantCategoryId { get; set; }
+		public string? OpeningTime { get; set; }
+		public string? ClosingTime { get; set; }
+	}
+}
--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Core.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-	internal class User
+	public class User:IEntity
 	{
+		public string? Id { get; set; }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+		public int Age { get; set; }
+		public string? Email { get; set; }
+		public string? Tc { get; set; }
+		public string? Address { get; set; }
+		public int PhoneNumber { get; set; }	
 	}
 }
