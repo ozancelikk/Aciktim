987c5a430b5d80d909c649488d7e787028b4cc90
yunusarslans <yunusemrearslan76@hotmail.com>
2023-03-29 22:16:54 +0300

    Entities Added

---
+++ Entities/Concrete/FoodCategory.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class FoodCategory : IEntity
+    {
+        public string Id { get; set; }
+        public string? FoodCategoryName { get; set; }
+        
+    }
+}
