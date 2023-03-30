e9f337f6bafd0411aa9b2699a3d70e2a78c701f4
Ozan <ozancelikk0808@gmail.com>
2023-03-30 13:08:00 +0300

    core Extensions
    core extensions klasörü yapıldı

---
+++ Core/Extensions/FormFileExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Extensions
+{
+    public static class FormFileExtensions
+    {
+        public static async Task<byte[]> GetBytes(this IFormFile formFile)
+        {
+            using (var ms = new MemoryStream())
+            {
+                await formFile.CopyToAsync(ms);
+                return ms.ToArray();
+            }
+        }
+    }
+}
