9e4d428e2978dce778672b61e38e973f4b236e48
Ozan <ozancelikk0808@gmail.com>
2023-03-30 11:20:27 +0300

    core katmanı
    Core katmanı eklentileri

---
+++ Core/DataAccess/Databases/IEntityRepository.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.Abstract;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Core.DataAccess.Databases
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        List<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter = null);
+        void Add(T entity);
+        DeleteResult Delete(T entity);
+        ReplaceOneResult Update(T entity);
+        DeleteResult DeleteMany(Expression<Func<T, bool>> filter = null);
+        List<T> GetAllWithPage(int page, int limit);
+    }
+}
