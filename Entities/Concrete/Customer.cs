ecb04819c6de888cd216820016356e8490b66721
Ozan <ozancelikk0808@gmail.com>
2023-03-30 14:51:27 +0300

    ProjectReset
    Reset To Project

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalityId { get; set; }
+        public string BirthDay { get; set; }
+        public string Address { get; set; }
+        public string Phonenumber { get; set; }
+        public string MailAddress { get; set; }
+    }
+}
---
+++ Entities/Concrete/Menu.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Menu:IEntity
+    {
+        public string Id { get; set; }
+        public string RestaurantId { get; set; }
+        public string MenuTitle { get; set; }
+        public string MenuDescription { get; set; }
+        public string MenuImage { get; set; }
+        public double MenuPrice { get; set; }
+        public List<string> MenuProperties { get; set; }
+
+    }
+}
