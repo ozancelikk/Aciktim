447b5bb52a750f43df201311a103b4c72a1d6278
Ozan <ozancelikk0808@gmail.com>
2023-03-30 10:26:56 +0300

    Entity
    entity kısmı düzenlendi

---
+++ Entities/Concrete/RestaurantFoodImage.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class RestaurantFoodImage:IEntity
+    {
+        public string Id { get; set; }
+        public string RestaurantMenuId { get; set; }
+        public string? ImagePath { get; set; }     
+    }
+}
