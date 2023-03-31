8897476c31a176d29b2f1af4e66bf72feae525bf
Yusuf Göktaş <yusufgk125@gmail.com>
2023-03-31 16:05:31 +0300

    User işlemleri
    User - UserOperationClaim - OperationClaim tabloları eklendi. JWT entegrasyonu yapıldı.

---
+++ Core/Entities/Concrete/DBEntities/OperationClaim.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Core.Entities.Concrete.DBEntities
+{
+    public class OperationClaim : IEntity
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int AuthorizationRate { get; set; }
+    }
+}
---
+++ DataAccess/Concrete/DataBases/MongoDB/MongoDB_UserOperationClaimDal.cs
@@ -0,0 +1,11 @@
+﻿using Core.DataAccess.Databases.MongoDB;
+using Core.Entities.Concrete.DBEntities;
+using DataAccess.Abstract;
+using DataAccess.Concrete.DataBases.MongoDB.Collections;
+
+namespace DataAccess.Concrete.DataBases.MongoDB
+{
+    public class MongoDB_UserOperationClaimDal : MongoDB_RepositoryBase<UserOperationClaim, MongoDB_Context<UserOperationClaim, MongoDB_UserOperationClaimCollection>>, IUserOperationClaimDal
+    {
+    }
+}
