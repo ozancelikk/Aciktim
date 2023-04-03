d8a33b9ecbba2daa9ce52c6dfcb337d9e7e6f0fe
Ozan <ozancelikk0808@gmail.com>
2023-04-03 12:28:56 +0300

    business
    auth servisleri yazıldı

---
+++ Business/Abstract/IRestaurantAuthService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities.Concrete.DBEntities;
+using Core.Utilities.Results;
+using Core.Utilities.Security.JWT;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IRestaurantAuthService
+    {
+        IDataResult<Restaurant> Register(RestaurantForRegisterDto restaurantForRegisterDto);
+        IDataResult<User> Login(RestaurantForLoginDto restaurantForLoginDto);
+        IResult UserExists(string email);
+        IDataResult<AccessToken> CreateAccessToken(Restaurant restaurant);
+        IResult ForgotPassword(string eMail);
+        IResult ChangeForgottenPassword(ForgettenPasswordForRestaurant forgettenPasswordForRestaurant);
+    }
+}
---
+++ Entities/Concrete/ForgettenPasswordForRestaurant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class ForgettenPasswordForRestaurant
+    {
+        public string Email { get; set; }
+        public string NewPassword { get; set; }
+        public string PrivateKey { get; set; }
+    }
+}
