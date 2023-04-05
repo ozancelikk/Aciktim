f67aea563ccd3d614d487a9b94d2ee59e5b47532
yunusarslans <yunusemrearslan76@hotmail.com>
2023-04-05 16:43:53 +0300

    admin api added

---
+++ AciktimAdminWebAPI/Controllers/CustomerOperationClaimController.cs
@@ -0,0 +1,92 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete.DBEntities;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AciktimAdminWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerOperationClaimController : ControllerBase
+    {
+        private readonly ICustomerOperationClaimService _customerOperationClaimService;
+
+        public CustomerOperationClaimController(ICustomerOperationClaimService customerOperationClaimService)
+        {
+            _customerOperationClaimService = customerOperationClaimService;
+        }
+
+        [HttpGet("getbyid")]
+
+        public IActionResult GetById(string id)
+        {
+            var result = _customerOperationClaimService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("getallclaims")]
+
+        public IActionResult GetAllClaims()
+        {
+            var result = _customerOperationClaimService.GetAllClaims();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult GetAll()
+        {
+            var result = _customerOperationClaimService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+        
+        public IActionResult Add(CustomerOperationClaimDto customerOperationClaimDto)
+        {
+            var result = _customerOperationClaimService.Add(customerOperationClaimDto);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+
+        public IActionResult Delete(CustomerOperationClaim ustomerOperationClaim)
+        {
+            var result = _customerOperationClaimService.Delete(ustomerOperationClaim);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+
+        public IActionResult Update(CustomerOperationClaim ustomerOperationClaim)
+        {
+            var result = _customerOperationClaimService.Update(ustomerOperationClaim);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
---
+++ AciktimAdminWebAPI/Controllers/RestaurantOperationClaimController.cs
@@ -0,0 +1,92 @@
+﻿using Business.Abstract;
+using Core.Entities.Concrete.DBEntities;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AciktimAdminWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantOperationClaimController : ControllerBase
+    {
+        private readonly IRestaurantOperationClaimService _restaurantOperationClaimService;
+
+        public RestaurantOperationClaimController(IRestaurantOperationClaimService restaurantOperationClaimService)
+        {
+            _restaurantOperationClaimService = restaurantOperationClaimService;
+        }
+
+        [HttpGet("getbyid")]
+
+        public IActionResult GetById(string id) 
+        {
+            var result = _restaurantOperationClaimService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getallcalims")]
+
+        public IActionResult GetAllClaims()
+        {
+            var result = _restaurantOperationClaimService.GetAllClaims();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult GetAll()
+        {
+            var result = _restaurantOperationClaimService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("add")]
+
+        public IActionResult Add(RestaurantOperationClaimDto restaurantOperationClaimDto)
+        {
+            var result = _restaurantOperationClaimService.Add(restaurantOperationClaimDto);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("update")]
+
+        public IActionResult Update(RestaurantOperationClaim restaurantOperationClaim)
+        {
+            var result = _restaurantOperationClaimService.Update(restaurantOperationClaim);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpPost("delete")]
+
+        public IActionResult Delete(RestaurantOperationClaim restaurantOperationClaim) 
+        {
+            var result = _restaurantOperationClaimService.Delete(restaurantOperationClaim);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
