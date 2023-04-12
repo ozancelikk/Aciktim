ad392c90289f06ec902a53214aef2dd22375d7a8
Ozan <ozancelikk0808@gmail.com>
2023-04-12 12:38:53 +0300

    favoriteRestaurant
    favori restoranlar için database tablosu oluşturuldu

---
+++ AciktimMusteriWebAPI/Controllers/FavoriteRestaurantController.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using Business.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AciktimMusteriWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FavoriteRestaurantController : ControllerBase
+    {
+        private readonly IFavoriteRestaurnatService _favoriteRestaurnatService;
+        private readonly IMapper _mapper;
+
+        public FavoriteRestaurantController(IFavoriteRestaurnatService favoriteRestaurnatService, IMapper mapper)
+        {
+            _favoriteRestaurnatService = favoriteRestaurnatService;
+            _mapper = mapper;
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(FavoriteRestaurantDetailsDto favoriteRestaurantDto)
+        {
+            var map = _mapper.Map<FavoriteRestaurant>(favoriteRestaurantDto);
+            var result=_favoriteRestaurnatService.Add(map);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetALl()
+        {
+            var result = _favoriteRestaurnatService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpGet("GetFavoriteRestaurantByCustomerID")]
+        public IActionResult GetFavoriteRestaurantByCustomerID(string id)
+        {
+            var result = _favoriteRestaurnatService.GetFavoriteRestaurantsByCustomerId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
---
+++ Business/Abstract/IFavoriteRestaurnatService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IFavoriteRestaurnatService
+    {
+        IResult Add(FavoriteRestaurant favouritere);
+        IResult Delete(string id);
+        IResult Update(FavoriteRestaurant favouritere);
+        IDataResult <List<FavoriteRestaurant>> GetAll();
+        IDataResult<List<FavoriteRestaurantDto>> GetFavoriteRestaurantsByCustomerId(string id);
+        IDataResult<FavoriteRestaurant> GetById(string id);
+    }
+}
